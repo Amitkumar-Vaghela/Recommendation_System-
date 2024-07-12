e1986097c42007e596c2a10c0cad31b800d352b1
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-12 21:14:46 +0530

    Update api_call.py

--- api_call.py
+++ api_call.py
@@ -2,12 +2,12 @@ import requests
 
 def get_user_data(user_id):
     # API call to get user data from music streaming service
-    response = requests.get(f'https://api.musicstreaming.com/users/{user_id}')
+    response = requests.get(f'https://musicbrainz.org/ws/2/recording?query=%22we%20will%20rock%20you%22%20AND%20arid:0383dadf-2a4e-4d10-a46a-e9e041da8eb3')
     user_data = response.json()
     return user_data
 
 def get_song_data():
     # API call to get song data from music streaming service
-    response = requests.get('https://api.musicstreaming.com/songs')
+    response = requests.get('https://musicbrainz.org/ws/2/recording?query=%22we%20will%20rock%20you%22%20AND%20arid:0383dadf-2a4e-4d10-a46a-e9e041da8eb3')
     song_data = response.json()
     return song_data
